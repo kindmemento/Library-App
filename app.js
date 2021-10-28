d81aca632311022aa1da114f58179c2de1c5b85e
kindmemento <kindmemento@gmail.com>
2021-10-28 14:17:14 +0300

    book_list.pug file created and filled

--- app.js
+++ app.js
@@ -30,6 +30,8 @@ app.use(express.static(path.join(__dirname, 'public')))
 app.use('/', indexRouter)
 app.use('/users', usersRouter)
 
+app.locals.format = require('date-fns/format')
+
 // catch 404 and forward to error handler
 app.use(function (req, res, next) {
 	next(createError(404))
