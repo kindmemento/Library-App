551b7e583b2d778d7876c4be0c2eba5d52ed5019
kindmemento <kindmemento@gmail.com>
2021-10-29 16:23:48 +0300

    author_create get & post methods implemented

--- controllers/authorController.js
+++ controllers/authorController.js
@@ -1,6 +1,7 @@
 const Author = require('../models/author')
 const Book = require('../models/book')
 const async = require('async')
+const { body, validationResult } = require('express-validator')
 
 //Display list of all Authors.
 exports.author_list = (req, res, next) => {
@@ -12,7 +13,7 @@ exports.author_list = (req, res, next) => {
 			}
 			res.render('author_list', {
 				title: 'Author List',
-				author_list: list_authors,
+				author_list: list_authors
 			})
 		})
 }
@@ -26,7 +27,7 @@ exports.author_detail = (req, res, next) => {
 			},
 			author_books: (callback) => {
 				Book.find({ author: req.params.id }, 'title summary').exec(callback)
-			},
+			}
 		},
 		(err, results) => {
 			if (err) {
@@ -40,7 +41,7 @@ exports.author_detail = (req, res, next) => {
 			res.render('author_detail', {
 				title: 'Author Detail',
 				author: results.author,
-				author_books: results.author_books,
+				author_books: results.author_books
 			})
 		}
 	)
@@ -48,13 +49,61 @@ exports.author_detail = (req, res, next) => {
 
 //Display Author create form on GET.
 exports.author_create_get = (req, res) => {
-	res.send('NOT IMPLEMENTED: Author create GET')
+	res.render('author_form', { title: 'Create Author' })
 }
 
 //Handle Author create on POST.
-exports.author_create_post = (req, res) => {
-	res.send('NOT IMPLEMENTED: Author create POST')
-}
+exports.author_create_post = [
+	body('first_name')
+		.trim()
+		.isLength({ min: 1 })
+		.escape()
+		.withMessage('First name must be specified.')
+		.isAlphanumeric()
+		.withMessage('First name has non-alphanumeric characters.'),
+	body('family_name')
+		.trim()
+		.isLength({ min: 1 })
+		.escape()
+		.withMessage('Family name must be specified.')
+		.isAlphanumeric()
+		.withMessage('Family name has non-alphanumeric characters.'),
+	body('date_of_birth', 'Invalid date of birth')
+		.optional({ checkFalsy: true })
+		.isISO8601()
+		.toDate(),
+	body('date_of_death', 'Invalid date of death')
+		.optional({ checkFalsy: true })
+		.isISO8601()
+		.toDate(),
+
+	(req, res, next) => {
+		const errors = validationResult(req)
+
+		if (!errors.isEmpty()) {
+			res.render('author_form', {
+				title: 'Create Author',
+				author: req.body,
+				errors: errors.array()
+			})
+			return
+		} else {
+			const author = new Author({
+				first_name: req.body.first_name,
+				family_name: req.body.family_name,
+				date_of_birth: req.body.date_of_birth,
+				date_of_death: req.body.date_of_death
+			})
+
+			author.save((err) => {
+				if (err) {
+					return next(err)
+				}
+				res.redirect(author.url)
+			})
+		}
+	}
+]
 
 //Display Author delete form on GET.
 exports.author_delete_get = (req, res) => {
