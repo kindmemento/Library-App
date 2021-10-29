687f7c1003691683f5e205085574c2a2e1a765d8
kindmemento <kindmemento@gmail.com>
2021-10-29 18:33:13 +0300

    book_create get & post methods implemented

--- controllers/bookController.js
+++ controllers/bookController.js
@@ -2,6 +2,7 @@ const Book = require('../models/book')
 const Author = require('../models/author')
 const Genre = require('../models/genre')
 const BookInstance = require('../models/bookinstance')
+const { body, validationResult } = require('express-validator')
 
 const async = require('async')
 
@@ -22,13 +23,13 @@ exports.index = (req, res) => {
 			},
 			genre_count: (callback) => {
 				Genre.countDocuments({}, callback)
-			},
+			}
 		},
 		(err, results) => {
 			res.render('index', {
 				title: 'Library Home',
 				error: err,
-				data: results,
+				data: results
 			})
 		}
 	)
@@ -45,7 +46,7 @@ exports.book_list = (req, res) => {
 			}
 			res.render('book_list', {
 				title: 'Book List',
-				book_list: list_books,
+				book_list: list_books
 			})
 		})
 }
@@ -63,7 +64,7 @@ exports.book_detail = (req, res, next) => {
 
 			book_instance: (callback) => {
 				BookInstance.find({ book: req.params.id }).exec(callback)
-			},
+			}
 		},
 		(err, results) => {
 			if (err) {
@@ -77,7 +78,7 @@ exports.book_detail = (req, res, next) => {
 			res.render('book_detail', {
 				title: results.book.title,
 				book: results.book,
-				book_instances: results.book_instance,
+				book_instances: results.book_instance
 			})
 		}
 	)
@@ -85,13 +86,111 @@ exports.book_detail = (req, res, next) => {
 
 //Display Book create form on GET.
 exports.book_create_get = (req, res) => {
-	res.send('NOT IMPLEMENTED: Book create GET')
+	async.parallel(
+		{
+			authors: (callback) => {
+				Author.find(callback)
+			},
+
+			genres: (callback) => {
+				Genre.find(callback)
+			}
+		},
+		(err, results) => {
+			if (err) {
+				return next(err)
+			}
+			res.render('book_form', {
+				title: 'Create Book',
+				authors: results.authors,
+				genres: results.genres
+			})
+		}
+	)
 }
 
 //Handle Book create on POST.
-exports.book_create_post = (req, res) => {
-	res.send('NOT IMPLEMENTED: Book create POST')
-}
+exports.book_create_post = [
+	(req, res, next) => {
+		if (!(req.body.genre instanceof Array)) {
+			if (typeof req.body.genre === 'undefined') {
+				req.body.genre = []
+			} else {
+				req.body.genre = new Array(req.body.genre)
+			}
+		}
+		next()
+	},
+
+	body('title', 'Title must not be empty.')
+		.trim()
+		.isLength({ min: 1 })
+		.escape(),
+	body('author', 'Author must not be empty.')
+		.trim()
+		.isLength({ min: 1 })
+		.escape(),
+	body('summary', 'Summary must not be empty.')
+		.trim()
+		.isLength({ min: 1 })
+		.escape(),
+	body('isbn', 'ISBN must not be empty')
+		.trim()
+		.isLength({ min: 1 })
+		.escape(),
+	body('genre.*').escape(),
+
+	(req, res, next) => {
+		const errors = validationResult(req)
+
+		const book = new Book({
+			title: req.body.title,
+			author: req.body.author,
+			summary: req.body.summary,
+			isbn: req.body.isbn,
+			genre: req.body.genre
+		})
+
+		if (!errors.isEmpty()) {
+			async.parallel(
+				{
+					authors: (callback) => {
+						Author.find(callback)
+					},
+					genres: (callback) => {
+						Genre.find(callback)
+					}
+				},
+				(err, results) => {
+					if (err) {
+						return next(err)
+					}
+
+					for (let i = 0; i < results.genres.length; i++) {
+						if (book.genre.indexOf(results.genres[i]._id) > -1) {
+							results.genres[i].checked = true
+						}
+					}
+					res.render('book_form', {
+						title: 'Create Book',
+						authors: results.authors,
+						genres: results.genres,
+						book: book,
+						errors: errors.array()
+					})
+				}
+			)
+			return
+		} else {
+			book.save((err) => {
+				if (err) {
+					return next(err)
+				}
+				res.redirect(book.url)
+			})
+		}
+	}
+]
 
 //Display Book delete form on GET.
 exports.book_delete_get = (req, res) => {
