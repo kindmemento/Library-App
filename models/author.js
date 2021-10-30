7f872c2cb813d05cfc22dc3fa1d74adfdbf6ad08
kindmemento <kindmemento@gmail.com>
2021-10-31 00:00:56 +0300

    redundant comments removed

--- models/author.js
+++ models/author.js
@@ -7,7 +7,7 @@ const AuthorSchema = new Schema({
 	first_name: { type: String, required: true, maxLenght: 100 },
 	family_name: { type: String, required: true, maxlength: 100 },
 	date_of_birth: { type: Date },
-	date_of_death: { type: Date },
+	date_of_death: { type: Date }
 })
 
 AuthorSchema.virtual('name').get(function () {
@@ -18,12 +18,10 @@ AuthorSchema.virtual('lifespan').get(function () {
 	let lifetime_string = ''
 	if (this.date_of_birth) {
 		lifetime_string = format(this.date_of_birth, 'PPP')
-		// lifetime_string = DateTime.fromJSDate(this.date_of_birth).toLocaleString(DateTime.DATE_MED)
 	}
 	lifetime_string += ' — '
 	if (this.date_of_death) {
 		lifetime_string += format(this.date_of_death, 'PPP')
-		// lifetime_string += DateTime.fromJSDate(this.date_of_death).toLocaleString(DateTime.DATE_MED)
 	}
 	return lifetime_string
 })
