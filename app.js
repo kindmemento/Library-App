997328fb8f4ec401873e67d3508ec8cb3e725786
kindmemento <kindmemento@gmail.com>
2021-10-31 16:32:54 +0300

    selected value of option corrected

--- app.js
+++ app.js
@@ -3,17 +3,18 @@ const express = require('express')
 const path = require('path')
 const cookieParser = require('cookie-parser')
 const logger = require('morgan')
+const compression = require('compression')
+const helmet = require('helmet')
 require('dotenv').config()
+
 const mongoose = require('mongoose')
 
-const password = process.env.password
-const mongoDB = `mongodb://kindmemento:${password}@library-shard-00-00.dkfde.mongodb.net:27017,library-shard-00-01.dkfde.mongodb.net:27017,library-shard-00-02.dkfde.mongodb.net:27017/Library?ssl=true&replicaSet=atlas-l8mhyg-shard-0&authSource=admin&retryWrites=true&w=majority`
+const mongoDB = process.env.MONGODB_URI
 mongoose.connect(mongoDB, { useNewUrlParser: true, useUnifiedTopology: true })
 const db = mongoose.connection
 db.on('error', console.error.bind(console, 'MongoDB connection error:'))
 
 const indexRouter = require('./routes/index')
-const usersRouter = require('./routes/users')
 const catalogRouter = require('./routes/catalog')
 
 const app = express()
@@ -22,6 +23,8 @@ const app = express()
 app.set('views', path.join(__dirname, 'views'))
 app.set('view engine', 'pug')
 
+app.use(compression())
+app.use(helmet())
 app.use(logger('dev'))
 app.use(express.json())
 app.use(express.urlencoded({ extended: false }))
@@ -29,10 +32,7 @@ app.use(cookieParser())
 app.use(express.static(path.join(__dirname, 'public')))
 
 app.use('/', indexRouter)
-app.use('/users', usersRouter)
 app.use('/catalog', catalogRouter)
-
-app.locals.format = require('date-fns/format')
 
 // catch 404 and forward to error handler
 app.use(function (req, res, next) {
